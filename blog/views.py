b3b87f22719d14ab34a9adba7ba7589132cf1e98
lukwac123 <lukasz-waclawek@wp.pl>
2024-10-10 13:47:28 +0200

    Utworzenie widoku wyświetlającego szczegóły posta.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import Http404
+
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("Nie znaleziono posta.")
+    
+    return render(request, 'blog/post/detail.html', {'post': post})
+
 
 def post_list(request):
     posts = Post.published.all()
