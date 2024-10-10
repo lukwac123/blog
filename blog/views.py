77fcd82f50534acddbf7dbe09b61990649e20935
lukwac123 <lukasz-waclawek@wp.pl>
2024-10-10 13:52:13 +0200

    Wykorzystanie get_object_or_404 zamiast wyjątku DoesNotExist.

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Nie znaleziono posta.")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     
     return render(request, 'blog/post/detail.html', {'post': post})
 
