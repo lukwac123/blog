a7c15e3f74434ae6ec81a4eb269a104f8eb7da29
lukwac123 <lukasz-waclawek@wp.pl>
2024-10-10 09:49:59 +0200

    Dodanie pola stanu dla tworzonego Posta, status Roboczy lub Opublikowany.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Roboczy'
+        PUBLISHED = 'PB', 'Opublikowany'
     
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
@@ -9,6 +13,7 @@ class Post(models.Model):
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']
