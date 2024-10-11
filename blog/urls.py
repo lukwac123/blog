91a86399a614c81003293772219689afe0717be9
lukwac123 <lukasz-waclawek@wp.pl>
2024-10-11 09:15:50 +0200

    Utworzenie szablonów dla widoków: bazowy, lista postów i szczegóły posta.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = 'blog'
-url_patterns = [
+urlpatterns = [
     # Widoki posta
     path('', views.post_list, name='post_list'),
     path('<int:id>/', views.post_detail, name='post_detail'),
