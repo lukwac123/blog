c02816e21446f12e636e1a9ccbec91025ab2489c
lukwac123 <lukasz-waclawek@wp.pl>
2024-10-11 18:00:13 +0200

    Utworzenie formularza i widoku do wysyłania postów poprzez email.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import EmailPostForms
+
+
+def post_share(request, post_id):
+    # Pobieranie posta wedlug identyfikatora.
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    if request.method == 'POST':
+        # Formularz został przesłany.
+        form = EmailPostForms(request.POST)
+        if form.is_valid():
+            # Pomyślnie zweryfikowano poprawność pól formularza.
+            cd = form.cleaned_data
+            # Wyślij email.
+        else:
+            form = EmailPostForms()
+        return render(request, 'blog/post/share.html', {'post':post, 'form':form})
 
 
 def post_detail(request, year, month, day, post):
