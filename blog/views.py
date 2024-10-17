d8febbc64dd6acf9f4ac3a0a1072abf999261dd5
lukwac123 <lukasz-waclawek@wp.pl>
2024-10-17 12:14:47 +0200

    Steamming i ranking wyników wyszukiwania.

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .forms import EmailPostForms, CommentForm, SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
@@ -91,6 +91,8 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'),).filter(search=query)
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)).filter(search=search_query).order_by('-rank')
     
     return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
